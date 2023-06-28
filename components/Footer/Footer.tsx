0ee8566a9b940c8660ac79dd316d2444b0a462b0
johnwayodi <wayodijohn@gmail.com>
2023-06-28 17:08:02 +0300

    update bg

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ interface Props { }
 
 const Footer: FC<Props> = (props) => {
   return (
-    <Row className="w-full bg-blue-900 h-16">
+    <Row className="w-full bg-teal-950 h-16">
       <Link href="https://github.com/johnwayodi/nextjs-rematch-tailwind-sample" target="blank" className="mx-auto mt-4">
         <p className="text-white">View on GitHub</p>
       </Link>
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ const { Title } = Typography;
 
 const NavBar: FC = () => {
   return (
-    <Row className="w-full bg-blue-900">
+    <Row className="w-full bg-teal-950">
       <Row className="w-full max-w-screen-2xl mx-auto px-4">
         <Link href="/">
           <h2 className="text-white text-2xl font-bold pt-4">On This Day</h2>
