1f6ee020fba54c66b33d3f979c4d45c1e54f4303
johnwayodi <wayodijohn@gmail.com>
2023-07-08 18:48:29 +0300

    update antd theme config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,20 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import { ConfigProvider, theme } from 'antd';
+import { ConfigProvider, theme } from "antd";
 
 import { Provider } from "react-redux";
 import { store } from "@/models";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <ConfigProvider
-        theme={{
-          algorithm: theme.darkAlgorithm,
-        }}
-      >
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
+      <Provider store={store}>
         <Component {...pageProps} />
-      </ConfigProvider>
-    </Provider>
+      </Provider>
+    </ConfigProvider>
   );
 }
